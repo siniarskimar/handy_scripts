93da34467007bc7268cb0694467c7ec4e20abc65
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2023-03-17 21:22:43 +0100

    TODO comment search utility

---
+++ search_todos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if command -v rg &> /dev/null
+then
+	rg 'TODO(.*):'
+elif command -v grep &> /dev/null
+	grep -rn 'TODO(.*):'
+else
+	printf 'grep and ripgrep are missing - cannot search\n'
+fi
